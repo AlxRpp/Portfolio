32a558c7f005b70f151a38602a69b83a96042785
AlxRpp <alexanderr26@outlook.de>
2025-06-21 19:23:38 +0200

    added first GSAP Animation

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,21 +1,59 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild, OnInit, signal } from '@angular/core';
+import { gsap, random } from "gsap";
+import { TextPlugin } from "gsap/TextPlugin";
+import { SplitText } from "gsap/SplitText";
+gsap.registerPlugin(TextPlugin, SplitText);
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule],
+  imports: [CommonModule,],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
-export class Header implements OnInit {
+export class Header implements OnInit, AfterViewInit {
+
+  @ViewChild('splitRef', { static: true }) splitRef!: ElementRef<HTMLDivElement>;
+
   name = signal<string>('Alex :)');
   topName = signal<string>('Hello world');
   isHovered = signal<boolean>(false);
   StartAnimation = signal<boolean>(false);
 
 
+  ngAfterViewInit(): void {
+    const split = SplitText.create(".split", { type: "chars, lines, words" });
+    const secondSplit = SplitText.create(".secondSplit", { type: "chars, lines, words" });
+
+    gsap.from(split.chars, {
+      duration: 2,
+      yPercent: "random(-100, 500)",
+      rotation: "random(-45, 45)",     // animate from 100px below
+      autoAlpha: 0, // fade in from opacity: 0 and visibility: hidden
+      stagger: {
+        amount: .3,
+        from: "random",
+        repeat: 0
+      } // 0.05 seconds between each
+    });
+
+    gsap.from(secondSplit.chars, {
+      duration: 2,
+      yPercent: "random(-100, 500)",
+      rotation: "random(-45, 45)",     // animate from 100px below
+      autoAlpha: 0, // fade in from opacity: 0 and visibility: hidden
+      stagger: {
+        amount: .3,
+        from: "random",
+        repeat: 0
+      } // 0.05 seconds between each
+    });
+  }
+
   ngOnInit(): void {
     this.mobileStartAnimation();
+
+
   }
 
   mouseEnter(): void {
@@ -37,4 +75,7 @@ export class Header implements OnInit {
       }, 2000)
     }
   }
+
+
+
 }
